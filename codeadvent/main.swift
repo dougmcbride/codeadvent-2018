ad27ce6fda7e6569b4258451861acf9069ce1e66
Doug McBride <doug.mcbride@disney.com>
2018-12-16 14:26:43 -0800

    2.2

--- codeadvent/main.swift
+++ codeadvent/main.swift
@@ -1,31 +1,40 @@
 import Foundation
 
-var totalTwos = 0
-var totalThrees = 0
+var ids = [String]()
+//var ids = ["adgwjcmbrkedihqtutfylyzpnx",
+//           "asgwjzmbrkeeihqputfylvzpnx",
+//           "csgwjzmbrkeeihqputfylvzpnx",
+//           "asgwjcmbrkwdihqoutfylvzpwc",
+//]
+
+var result: String?
 
 while let idString = readLine(strippingNewline: true) {
-    var hasTwo = false
-    var hasThree = false
-    
-    let countedLetterSet = NSCountedSet(array: Array(idString))
-
-    nextSet: for letter in countedLetterSet.objectEnumerator() {
-        switch countedLetterSet.count(for: letter) {
-        case 2: hasTwo = true
-        case 3: hasThree = true
-        default: break
-        }
-        
-        if hasTwo && hasThree {
-            break
-        }
-    }
-    
-    totalTwos += hasTwo ? 1 : 0
-    totalThrees += hasThree ? 1 : 0
+    ids.append(idString)
 }
 
-print("2's: \(totalTwos)")
-print("3's: \(totalThrees)")
-print("answer: \(totalTwos * totalThrees)")
+outerLoop: for id1 in ids {
+    innerLoop: for id2 in ids {
+        if id1 == id2 {
+            continue
+        }
+        
+        var indexOfDifference: String.Index?
+
+        for index in id1.indices {
+            if id1[index] != id2[index] {
+                if indexOfDifference == nil {
+                    indexOfDifference = index
+                } else {
+                    continue innerLoop
+                }
+            }
+        }
+
+        result = String(id1[..<indexOfDifference!]) + String(id1[id1.index(after: indexOfDifference!)...])
+        break outerLoop
+    }
+}
+
+print(result!)
         
