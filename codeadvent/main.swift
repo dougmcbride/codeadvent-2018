218a18743e23857105453c86bb783c28f88a36f0
Doug McBride <doug.mcbride@disney.com>
2019-01-02 10:37:32 -0800

    11.1

--- codeadvent/main.swift
+++ codeadvent/main.swift
@@ -1,111 +1,53 @@
 import Foundation
 
-func int(_ substring: Substring) -> Int {
-    return Int(substring.trimmingCharacters(in: .whitespaces))!
-}
+struct Cell {
+    let position: (Int, Int)
+    let serialNumber: Int
 
-struct Vector {
-    let x, y: Int
-
-    init(_ x: Int, _ y: Int) {
-        self.x = x
-        self.y = y
+    var power: Int {
+        // Find the fuel cell's rack ID, which is its X coordinate plus 10.
+        let rackId = position.0 + 10
+        //Begin with a power level of the rack ID times the Y coordinate.
+        var p = rackId * position.1
+        //Increase the power level by the value of the grid serial number (your puzzle input).
+        p += serialNumber
+        //Set the power level to itself multiplied by the rack ID.
+        p *= rackId
+        //Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+        p = p / 100 % 10
+        //Subtract 5 from the power level.
+        p -= 5
+        return p
     }
 }
 
-struct Point {
-    var position: Vector
-    var velocity: Vector
-    
-    init(position: Vector, velocity: Vector) {
-        self.position = position
-        self.velocity = velocity
-    }
+func main(serialNumber: Int) -> (Int, Int, Int) {
+    var startingPosition: (Int, Int)?
+    var maxSize: Int?
+    var maxPower = Int.min
 
-    init(string: String) {
-        let pWidth = 6
-        let vWidth = 2
-        let pxStart = string.index(string.startIndex, offsetBy: "position=<".count)
-        let pxEnd = string.index(pxStart, offsetBy: pWidth)
-        let pyStart = string.index(pxEnd, offsetBy: 2)
-        let pyEnd = string.index(pyStart, offsetBy: pWidth)
-        let vxStart = string.index(pyEnd, offsetBy: "> velocity=<".count)
-        let vxEnd = string.index(vxStart, offsetBy: vWidth)
-        let vyStart = string.index(vxEnd, offsetBy: 2)
-        let vyEnd = string.index(vyStart, offsetBy: vWidth)
+    for size in 1...300 {
+        print(size)
+        for x in 0...300 - size {
+            for y in 0...300 - size {
+                var totalPower = 0
 
-        position = Vector(int(string[pxStart..<pxEnd]),
-                          int(string[pyStart..<pyEnd]))
-        velocity = Vector(int(string[vxStart..<vxEnd]),
-                          int(string[vyStart..<vyEnd]))
-    }
-    
-    init(substring: Substring) {
-        self.init(string: String(substring))
-    }
-    
-}
+                for dx in 0..<size {
+                    for dy in 0..<size {
+                        totalPower += Cell(position: (x + dx, y + dy), serialNumber: serialNumber).power
+                    }
+                }
 
-class Sky {
-    var lights: [Point] = []
-
-    init(lights: [Point]) {
-        self.lights = lights
-    }
-
-    var isLetters: Bool {
-        return lights.reduce(into: true) { result, light in result = result && hasNeighbor(light) }
-    }
-
-    func moveLights() {
-        for (index, light) in lights.enumerated() {
-            lights[index] = Point(position: Vector(light.position.x + light.velocity.x,
-                                                   light.position.y + light.velocity.y),
-                                  velocity: light.velocity)
-        }
-    }
-    private func hasNeighbor(_ light: Point) -> Bool {
-        for dx in -1...1 {
-            for dy in -1...1 {
-                guard dx != 0 || dy != 0 else { continue }
-                if lights.contains(where: {
-                    return $0.position.x == light.position.x + dx && $0.position.y == light.position.y + dy
-                }) {
-                    return true
+                if totalPower > maxPower {
+                    maxPower = totalPower
+                    startingPosition = (x, y)
+                    maxSize = size
                 }
             }
         }
-        return false
-    }
-}
-
-extension Sky: CustomStringConvertible {
-    public var description: String {
-        let minX = lights.map { $0.position.x }.min()!
-        let maxX = lights.map { $0.position.x }.max()!
-        let minY = lights.map { $0.position.y }.min()!
-        let maxY = lights.map { $0.position.y }.max()!
-
-        var string = ""
-        for y in minY...maxY {
-            for x in minX...maxX {
-                string += lights.contains(where: { $0.position.x == x && $0.position.y == y }) ?
-                    "#" : " "
-            }
-            string += "\n"
-        }
-        return string
-    }
-}
-
-func main(lights: [Point]) -> String {
-    let sky = Sky(lights: lights)
-
-    while !sky.isLetters {
-        sky.moveLights()
     }
 
-    return sky.description
+    return (startingPosition!.0, startingPosition!.1, maxSize!)
 }
 
 //let inputString = "position=< 9,  1> velocity=< 0,  2>\nposition=< 7,  0> velocity=<-1,  0>\nposition=< 3, -2> velocity=<-1,  1>\nposition=< 6, 10> velocity=<-2, -1>\nposition=< 2, -4> velocity=< 2,  2>\nposition=<-6, 10> velocity=< 2, -2>\nposition=< 1,  8> velocity=< 1, -1>\nposition=< 1,  7> velocity=< 1,  0>\nposition=<-3, 11> velocity=< 1, -2>\nposition=< 7,  6> velocity=<-1, -1>\nposition=<-2,  3> velocity=< 1,  0>\nposition=<-4,  3> velocity=< 2,  0>\nposition=<10, -3> velocity=<-1,  1>\nposition=< 5, 11> velocity=< 1, -2>\nposition=< 4,  7> velocity=< 0, -1>\nposition=< 8, -2> velocity=< 0,  1>\nposition=<15,  0> velocity=<-2,  0>\nposition=< 1,  6> velocity=< 1,  0>\nposition=< 8,  9> velocity=< 0, -1>\nposition=< 3,  3> velocity=<-1,  1>\nposition=< 0,  5> velocity=< 0, -1>\nposition=<-2,  2> velocity=< 2,  0>\nposition=< 5, -2> velocity=< 1,  2>\nposition=< 1,  4> velocity=< 2,  1>\nposition=<-2,  7> velocity=< 2, -2>\nposition=< 3,  6> velocity=<-1, -1>\nposition=< 5,  0> velocity=< 1,  0>\nposition=<-6,  0> velocity=< 2,  0>\nposition=< 5,  9> velocity=< 1, -2>\nposition=<14,  7> velocity=<-2,  0>\nposition=<-3,  6> velocity=< 2, -1>\n"
@@ -118,10 +60,11 @@ func main(lights: [Point]) -> String {
 //var lines = [String]()
 //
 
-var lights = [Point]()
-while let line = readLine(strippingNewline: true) {
-    lights.append(Point(string: line))
-}
+//var lights = [Point]()
+//while let line = readLine(strippingNewline: true) {
+//    lights.append(Point(string: line))
+//}
 
-print(main(lights: lights))
+//print(main(serialNumber: 42))
+print(main(serialNumber: 1309))
 
