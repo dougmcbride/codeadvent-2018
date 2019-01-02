05181353e26a617dc0ebb984b8ebdc71114a6e2a
Doug McBride <doug.mcbride@disney.com>
2019-01-01 20:41:24 -0800

    9.1

--- codeadvent/main.swift
+++ codeadvent/main.swift
@@ -1,88 +1,68 @@
 import Foundation
 
-struct Tree {
-    let root: Node
+extension Array {
+    func marble(at index: Int) -> Element {
+        return self[trueIndex(of: index)]
+    }
+
+    mutating func placeMarble(marble: Element, at index: Int) -> Int {
+        let realIndex = trueIndex(of: index)
+        self.insert(marble, at: realIndex)
+        return realIndex
+    }
+
+    mutating func removeMarble(at index: Int) -> Element {
+        return self.remove(at: trueIndex(of: index))
+    }
+
+    private func trueIndex(of index: Int) -> Int {
+        return (index < 0 ? count - index : index) % count
+    }
 }
 
-class Node: Hashable, Named {
-    private static var id = 0
+typealias Marble = Int
+typealias Score = Int
 
-    let id: Int
-    let size: Int
-    let metadata: [Int]
-    let children: [Node]
-
-    var name: String {
-        return String(id)
+func tostring(circle: [Marble], currentIndex: Int) -> String {
+    var c = 0
+    return circle.reduce(into: "") { result, marble in
+        result += String(format: (c == currentIndex) ?
+            "(%02i)" : " %02i ", marble)
+        c += 1
     }
+}
 
-    var duration: Int {
-        return 60 + Int(Unicode.Scalar(name)!.value - Unicode.Scalar("A")!.value + 1)
-    }
+func main(playerCount: Int, lastMarble: Marble) -> Score {
+    var scores = Array<Score>(repeating: 0, count: playerCount)
+    var circle = [0]
+    var currentIndex = 0
+    var currentPlayer = 0
 
-    init(data: Array<Int>) {
-        self.id = Node.id
-        Node.id += 1
-
-        var size = 2
-
-        let childCount = data[0]
-        let metadataCount = data[1]
-
-        var children = [Node]()
-        for _ in 0..<childCount {
-            let child = Node(data: data[size...])
-            children.append(child)
-            size += child.size
+//    let percent: Int = lastMarble / 100
+    for marble in 1...lastMarble {
+//        if marble % percent == 0 {
+//            print(marble / percent)
+//        }
+        if marble % 23 == 0 {
+//            print("score \(scores[currentPlayer]) -> ", terminator: "")
+            scores[currentPlayer] += marble
+            currentIndex -= 7
+            let marble2: Int = circle.removeMarble(at: currentIndex)
+            scores[currentPlayer] += marble2
+            print("\(marble) and \(marble2)")
+//            print(scores[currentPlayer])
+        } else {
+            let newIndex = currentIndex + 2
+            currentIndex = circle.placeMarble(marble: marble, at: newIndex)
         }
-        self.children = children
 
-        self.size = size + metadataCount
-
-        self.metadata = Array(data[size..<self.size])
+        currentPlayer += 1
+        currentPlayer %= playerCount
+        currentIndex %= circle.count
+//        print(tostring(circle: circle, currentIndex: currentIndex))
     }
 
-    convenience init(data: ArraySlice<Int>) {
-        self.init(data: Array(data))
-    }
-
-    var metadataSum: Int {
-        return metadata.reduce(0, +) + children.reduce(into: 0) { $0 += $1.metadataSum }
-    }
-
-    var value: Int {
-        let empty = children.isEmpty
-        return empty ?
-            metadataSum :
-            metadata.reduce(into: 0) { result, childIndex in
-                if (children.startIndex..<children.endIndex).contains(childIndex - 1) {
-                    result += children[childIndex - 1].value
-                }
-            }
-    }
-}
-
-protocol Named: CustomStringConvertible, Hashable {
-    var name: String { get }
-}
-
-extension Named {
-    public var description: String {
-        return name
-    }
-
-    var hashValue: Int {
-        return name.hashValue
-    }
-
-    public static func ==(lhs: Self, rhs: Self) -> Bool {
-        return lhs.name == rhs.name
-    }
-}
-
-func main(data: [Int]) -> Int {
-    let rootNode = Node(data: data)
-    return rootNode.value
+    return scores.max()!
 }
 
 //let inputString = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
@@ -91,11 +71,20 @@ func main(data: [Int]) -> Int {
 //    .split(separator: " ")
 //    .map { Int($0)! }
 
-var input = [Int]()
+//var input = [Int]()
 //var lines = [String]()
 //
-while let line = readLine(strippingNewline: true) {
-    input.append(contentsOf: line.split(separator: " ").map { Int($0)! })
-}
+//while let line = readLine(strippingNewline: true) {
+//    input.append(contentsOf: line.split(separator: " ").map { Int($0)! })
+//}
 
-print(main(data: input))
+//print(main(playerCount: 9, lastMarble: 96))
+//print(main(playerCount: 10, lastMarble: 1618))
+//print(main(playerCount: 13, lastMarble: 7999))
+//print(main(playerCount: 17, lastMarble: 1104))
+//print(main(playerCount: 21, lastMarble: 6111))
+//print(main(playerCount: 30, lastMarble: 5807))
+//print()
+print(main(playerCount: 418, lastMarble: 71339))
+//print(main(playerCount: 418, lastMarble: 7133900))
+
