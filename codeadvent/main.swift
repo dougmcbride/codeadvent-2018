9311aa95adf8c2e605cec0e58349cf9b3d302558
Doug McBride <doug.mcbride@disney.com>
2019-01-02 10:08:52 -0800

    10.1

--- codeadvent/main.swift
+++ codeadvent/main.swift
@@ -1,90 +1,127 @@
 import Foundation
 
-extension Array {
-    func marble(at index: Int) -> Element {
-        return self[trueIndex(of: index)]
-    }
+func int(_ substring: Substring) -> Int {
+    return Int(substring.trimmingCharacters(in: .whitespaces))!
+}
 
-    mutating func placeMarble(marble: Element, at index: Int) -> Int {
-        let realIndex = trueIndex(of: index)
-        self.insert(marble, at: realIndex)
-        return realIndex
-    }
+struct Vector {
+    let x, y: Int
 
-    mutating func removeMarble(at index: Int) -> Element {
-        return self.remove(at: trueIndex(of: index))
-    }
-
-    private func trueIndex(of index: Int) -> Int {
-        return (index < 0 ? count - index : index) % count
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
     }
 }
 
-typealias Marble = Int
-typealias Score = Int
-
-func tostring(circle: [Marble], currentIndex: Int) -> String {
-    var c = 0
-    return circle.reduce(into: "") { result, marble in
-        result += String(format: (c == currentIndex) ?
-            "(%02i)" : " %02i ", marble)
-        c += 1
+struct Point {
+    var position: Vector
+    var velocity: Vector
+    
+    init(position: Vector, velocity: Vector) {
+        self.position = position
+        self.velocity = velocity
     }
+
+    init(string: String) {
+        let pWidth = 6
+        let vWidth = 2
+        let pxStart = string.index(string.startIndex, offsetBy: "position=<".count)
+        let pxEnd = string.index(pxStart, offsetBy: pWidth)
+        let pyStart = string.index(pxEnd, offsetBy: 2)
+        let pyEnd = string.index(pyStart, offsetBy: pWidth)
+        let vxStart = string.index(pyEnd, offsetBy: "> velocity=<".count)
+        let vxEnd = string.index(vxStart, offsetBy: vWidth)
+        let vyStart = string.index(vxEnd, offsetBy: 2)
+        let vyEnd = string.index(vyStart, offsetBy: vWidth)
+
+        position = Vector(int(string[pxStart..<pxEnd]),
+                          int(string[pyStart..<pyEnd]))
+        velocity = Vector(int(string[vxStart..<vxEnd]),
+                          int(string[vyStart..<vyEnd]))
+    }
+    
+    init(substring: Substring) {
+        self.init(string: String(substring))
+    }
+    
 }
 
-func main(playerCount: Int, lastMarble: Marble) -> Score {
-    var scores = Array<Score>(repeating: 0, count: playerCount)
-    var circle = [0]
-    var currentIndex = 0
-    var currentPlayer = 0
+class Sky {
+    var lights: [Point] = []
 
-//    let percent: Int = lastMarble / 100
-    for marble in 1...lastMarble {
-//        if marble % percent == 0 {
-//            print(marble / percent)
-//        }
-        if marble % 23 == 0 {
-//            print("score \(scores[currentPlayer]) -> ", terminator: "")
-            scores[currentPlayer] += marble
-            currentIndex -= 7
-            let marble2: Int = circle.removeMarble(at: currentIndex)
-            scores[currentPlayer] += marble2
-            print("\(marble) and \(marble2)")
-//            print(scores[currentPlayer])
-        } else {
-            let newIndex = currentIndex + 2
-            currentIndex = circle.placeMarble(marble: marble, at: newIndex)
+    init(lights: [Point]) {
+        self.lights = lights
+    }
+
+    var isLetters: Bool {
+        return lights.reduce(into: true) { result, light in result = result && hasNeighbor(light) }
+    }
+
+    func moveLights() {
+        for (index, light) in lights.enumerated() {
+            lights[index] = Point(position: Vector(light.position.x + light.velocity.x,
+                                                   light.position.y + light.velocity.y),
+                                  velocity: light.velocity)
         }
-
-        currentPlayer += 1
-        currentPlayer %= playerCount
-        currentIndex %= circle.count
-//        print(tostring(circle: circle, currentIndex: currentIndex))
     }
-
-    return scores.max()!
+    private func hasNeighbor(_ light: Point) -> Bool {
+        for dx in -1...1 {
+            for dy in -1...1 {
+                guard dx != 0 || dy != 0 else { continue }
+                if lights.contains(where: {
+                    return $0.position.x == light.position.x + dx && $0.position.y == light.position.y + dy
+                }) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
 }
 
-//let inputString = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+extension Sky: CustomStringConvertible {
+    public var description: String {
+        let minX = lights.map { $0.position.x }.min()!
+        let maxX = lights.map { $0.position.x }.max()!
+        let minY = lights.map { $0.position.y }.min()!
+        let maxY = lights.map { $0.position.y }.max()!
 
+        var string = ""
+        for y in minY...maxY {
+            for x in minX...maxX {
+                string += lights.contains(where: { $0.position.x == x && $0.position.y == y }) ?
+                    "#" : " "
+            }
+            string += "\n"
+        }
+        return string
+    }
+}
+
+func main(lights: [Point]) -> String {
+    let sky = Sky(lights: lights)
+
+    while !sky.isLetters {
+        sky.moveLights()
+    }
+
+    return sky.description
+}
+
+//let inputString = "position=< 9,  1> velocity=< 0,  2>\nposition=< 7,  0> velocity=<-1,  0>\nposition=< 3, -2> velocity=<-1,  1>\nposition=< 6, 10> velocity=<-2, -1>\nposition=< 2, -4> velocity=< 2,  2>\nposition=<-6, 10> velocity=< 2, -2>\nposition=< 1,  8> velocity=< 1, -1>\nposition=< 1,  7> velocity=< 1,  0>\nposition=<-3, 11> velocity=< 1, -2>\nposition=< 7,  6> velocity=<-1, -1>\nposition=<-2,  3> velocity=< 1,  0>\nposition=<-4,  3> velocity=< 2,  0>\nposition=<10, -3> velocity=<-1,  1>\nposition=< 5, 11> velocity=< 1, -2>\nposition=< 4,  7> velocity=< 0, -1>\nposition=< 8, -2> velocity=< 0,  1>\nposition=<15,  0> velocity=<-2,  0>\nposition=< 1,  6> velocity=< 1,  0>\nposition=< 8,  9> velocity=< 0, -1>\nposition=< 3,  3> velocity=<-1,  1>\nposition=< 0,  5> velocity=< 0, -1>\nposition=<-2,  2> velocity=< 2,  0>\nposition=< 5, -2> velocity=< 1,  2>\nposition=< 1,  4> velocity=< 2,  1>\nposition=<-2,  7> velocity=< 2, -2>\nposition=< 3,  6> velocity=<-1, -1>\nposition=< 5,  0> velocity=< 1,  0>\nposition=<-6,  0> velocity=< 2,  0>\nposition=< 5,  9> velocity=< 1, -2>\nposition=<14,  7> velocity=<-2,  0>\nposition=<-3,  6> velocity=< 2, -1>\n"
+//
 //let input = inputString
-//    .split(separator: " ")
-//    .map { Int($0)! }
+//    .split(separator: "\n")
+//    .map{Point.init(substring: $0)}
 
 //var input = [Int]()
 //var lines = [String]()
 //
-//while let line = readLine(strippingNewline: true) {
-//    input.append(contentsOf: line.split(separator: " ").map { Int($0)! })
-//}
 
-//print(main(playerCount: 9, lastMarble: 96))
-//print(main(playerCount: 10, lastMarble: 1618))
-//print(main(playerCount: 13, lastMarble: 7999))
-//print(main(playerCount: 17, lastMarble: 1104))
-//print(main(playerCount: 21, lastMarble: 6111))
-//print(main(playerCount: 30, lastMarble: 5807))
-//print()
-print(main(playerCount: 418, lastMarble: 71339))
-//print(main(playerCount: 418, lastMarble: 7133900))
+var lights = [Point]()
+while let line = readLine(strippingNewline: true) {
+    lights.append(Point(string: line))
+}
+
+print(main(lights: lights))
 
