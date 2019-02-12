c522ca7e890d23acecc6963a804743dafc3dd79e
Doug McBride <doug.mcbride@disney.com>
2019-02-12 14:27:51 -0800

    Clean up solution

--- codeadvent/main.swift
+++ codeadvent/main.swift
@@ -1,37 +1,20 @@
-import Foundation
+var frequencyChanges = [Int]()
 
-struct Input {
-    let intValue: Int
+while let frequencyChangeString = readLine(strippingNewline: true) {
+    frequencyChanges.append(Int(frequencyChangeString)!)
+}
 
-    init(_ s: String) {
-        let negative = s.starts(with: "-")
-        let multiplier = negative ? -1 : 1
-        intValue = Int(s.dropFirst(1))! * multiplier
+var frequenciesSeen = Set<Int>()
+var currentFrequency = 0
+
+for change in frequencyChanges {
+    if frequenciesSeen.contains(currentFrequency) {
+        break
     }
+    
+    frequenciesSeen.insert(currentFrequency)
+    currentFrequency += change
 }
 
-var inputs = [Input]()
-var seen = Set<Int>()
-var current = 0
-var result: Int?
-
-while let inString = readLine(strippingNewline: true) {
-    inputs.append(Input(inString))
-}
-
-while result == nil {
-    for input in inputs {
-        seen.insert(current)
-        print(seen)
-
-        current += input.intValue
-
-        if seen.contains(current) {
-            result = current
-            break
-        }
-    }
-}
-
-print("answer: \(result!)")
+print("answer: \(currentFrequency)")
         
